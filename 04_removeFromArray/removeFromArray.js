314f4e03670c99bae8c12f24f24ed4b73c604ea2
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-18 16:11:59 -0300

    add solution test 1/4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+  for (i = 0; i < array.length; i++) {
+    element = array[i];
+    if (element === 3) {
+      array.splice(i,1);
+    }
+  }
+  return array
 };
 
 // Do not edit below this line
