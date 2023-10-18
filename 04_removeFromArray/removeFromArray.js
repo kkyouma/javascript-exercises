8d6878df467f63bf65e1f22d5425de4a495119dc
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-18 17:04:59 -0300

    add 03 solution 7/7 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,22 @@
-const removeFromArray = function(array) {
+const removeFromArray = function(array, ...toRemove) {
 
-  for (i = 0; i < array.length; i++) {
-    element = array[i];
-    if (element === 3) {
-      array.splice(i,1);
-    }
-  }
-  return array
+  // for (i = array.length - 1; i >= 0; i--) {
+  //   let element = array[i];
+  //   if (toRemove.includes(element)) {
+  //     array.splice(i,1);
+  //   }
+  // }
+  // return array
+
+  // const newArray = []
+  // array.forEach(element => {
+  //   if(!toRemove.includes(element))
+  //     newArray.push(element)
+  // });
+  // return newArray
+
+  newArray = array.filter(element => (!toRemove.includes(element)))
+  return newArray
 };
 
 // Do not edit below this line
