de908fe4c701f9722f0ee5d8a98930cfa22f4746
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-18 15:54:35 -0300

    clean 02 solution 7/7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,16 +1,11 @@
 const repeatString = function(word, times) {
-    let concatWords = ""
+    let concatWords = "";
 
-    if(times < 0) {
-        return "ERROR"
-
-    } else {
+    if(times < 0) return "ERROR";
     for (let i = 0; i < times; i++) {
     concatWords += word;
     }
-    
     return concatWords
-    }
 };
 
 // Do not edit below this line
