53bc38fe925ee062069a4f91eb7c4c31f8219b03
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-18 20:01:38 -0300

    add 06 solution 6/6 passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// divisibles por 4
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true
+  } else if (year % 400 === 0) {
+    return true
+  } else 
+    return false
+//divisibles por 100
+  // pero si son divisibles por 400 estan bien
 };
 
 // Do not edit below this line
