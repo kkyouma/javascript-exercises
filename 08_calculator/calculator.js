eb23c118f93334f7f75b60f30c136f3a53392ece
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-10-19 20:44:49 -0300

    add 08 solution 16/16 passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	 return firstNumber + secondNumber
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber - secondNumber
 };
 
-const sum = function() {
-	
+const sum = function(arrayNumbers) {
+  let total = 0;
+  for (number of arrayNumbers) {
+    total += parseFloat(number);
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(arrayNumbers)  {
+  let total = 1;
+  for (number of arrayNumbers) {
+    total *= parseFloat(number);
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(base, powerTo) {
+  let total = 1;
+  for (i = 0; i < powerTo; i++) {
+    total *= parseFloat(base);
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+  for (i = 0; i < number; i++) {
+    total *= (number - i)
+  }
+  return total 
 };
 
 // Do not edit below this line
