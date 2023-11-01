2a22aeee6554e53842a9cf14937d8769330c9ca3
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-01 06:28:24 -0300

    add 1 test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+// invertir cadena
+newString = string.split('').reverse().join('')
+// crear comprobar si la cadena invertirda es ===  a la cadena inicial
+return newString === string
 };
 
 // Do not edit below this line
