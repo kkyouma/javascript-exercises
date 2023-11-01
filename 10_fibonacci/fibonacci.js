37bcc74456a2390258829e1feb4ec0ac10d1cf4b
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-01 19:33:31 -0300

    add array secuence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+// Crear secuencia y almacenar en una array
 
+let secuence = [0, 1]
+for(i = 2; i < n; i++) {
+  let next = secuence[i-1] + secuence[i-2];
+  secuence.push(next)
+}
+
+// Seleccionar el indice de la array
+return secuence[i]
 };
 
 // Do not edit below this line
