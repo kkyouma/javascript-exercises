0bea895f761713ec0bf8a9b84aaab54c892e6f39
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-01 20:06:29 -0300

    clean up

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,13 @@
 const fibonacci = function(n) {
 
-  let secuence = [0, 1]
   if (n < 0) return "OOPS";
-
+  
+  let secuence = [0, 1]
   for(i = 2; i <= n; i++) {
     let next = secuence[i-1] + secuence[i-2];
     secuence.push(next)
 }
- return secuence[n]
+ return secuence[n];
 };
 // Do not edit below this line
 module.exports = fibonacci;
