46b949e6f0118b2eb64b0a026fededaa4bc81fcf
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-01 06:08:59 -0300

    clean up

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,19 +3,15 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
 const sum = function(array) {
-  return array.reduce((total, num) => total + num, 0)
-}
+  return array.reduce((total, num) => total + num, 0);
+};
 
 const multiply = function(array) {
-  return array.reduce((product, num) => product * num, 1)
-}
+  return array.reduce((product, num) => product * num, 1);
+};
 
-const power = function(base, powerTo) {
-  let total = 1;
-  for (i = 0; i < powerTo; i++) {
-    total *= parseFloat(base);
-  }
-  return total
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
 const factorial = function(number) {
