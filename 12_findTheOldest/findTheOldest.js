92c2e5724808a096486b58befa8538ea0b44c48f
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-01 21:35:30 -0300

    add test 3/3 passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+      const firstAge = 'yearOfDeath' in person ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
+      const secondAge = 'yearOfDeath' in oldest ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth;
+      return firstAge > secondAge ? person : oldest;
+    }); 
 };
 
 // Do not edit below this line
